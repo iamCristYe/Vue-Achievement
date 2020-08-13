aa8e921ef8113a5928925bcd4aa3db613399a856
Crist Ye <iamCristYe@Outlook.com>
2020-08-13 16:06:55 +0800

    data modified and png added

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,89 @@
 <template>
   <v-app>
-    <v-app-bar app color="#DDA300" dark>
-      <v-spacer></v-spacer>
-      <v-toolbar-title>
-        {{
-        username === "" ? me + org : username + org
-        }}
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-app-bar>
-    <v-main>
-      <v-container class="fill-height" fluid>
-        <v-col v-for="achievement in achievements" :key="achievement.id" cols="12">
-          <v-card
-            class="mx-auto"
-            max-width="344"
-            outlined
-            @[clickable]="achievement.selected = !achievement.selected"
-            :color="achievement.selected ? achievement.color : 'white'"
-          >
-            <v-list-item three-line>
-              <v-list-item-content>
-                <v-list-item-title class="headline mb-1">{{ achievement.title }}</v-list-item-title>
-                <v-list-item-subtitle>{{ achievement.description }}</v-list-item-subtitle>
-              </v-list-item-content>
-              <v-list-item-avatar tile v-if="achievement.selected">{{ achievement.emoji }}</v-list-item-avatar>
-            </v-list-item>
-          </v-card>
-        </v-col>
-        <v-col cols="12" sm="6" md="3">
-          <v-text-field
-            v-if="!viewonly"
-            v-model.trim="username"
-            :label="you+name"
-            :placeholder="namenotsent"
-          ></v-text-field>
-        </v-col>
-        <div>
-          <v-btn v-if="!viewonly" rounded color="primary" dark @click="changeParam">确定</v-btn>
-        </div>
-        <div>
-          <v-btn v-if="viewonly" rounded color="primary" dark @click="resetForm">制作我的</v-btn>
-        </div>
-      </v-container>
-    </v-main>
+    <v-overlay v-if="pageStatus=='ImgSharePageProcessing'">生成图片中</v-overlay>
+    <v-flex id="forShare">
+      <v-app-bar app color="#DDA300" dark>
+        <v-spacer></v-spacer>
+        <v-toolbar-title>
+          {{
+          username === "" ? me + org : username + org
+          }}
+        </v-toolbar-title>
+        <vue-title :title="username === '' ? me + org : username + org"></vue-title>
+        <v-spacer></v-spacer>
+      </v-app-bar>
+      <v-main v-if="pageStatus!='ImgSharePage'">
+        <v-container class="fill-height" fluid>
+          <v-col v-for="achievement in achievements" :key="achievement.id" cols="12">
+            <v-card
+              class="mx-auto"
+              max-width="344"
+              outlined
+              @[clickable]="achievement.selected = !achievement.selected"
+              :color="achievement.selected ? achievement.color : 'white'"
+            >
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="headline mb-1">{{ achievement.title }}</v-list-item-title>
+                  <v-list-item-subtitle>{{ achievement.description }}</v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-avatar tile v-if="achievement.selected">{{ achievement.emoji }}</v-list-item-avatar>
+              </v-list-item>
+            </v-card>
+          </v-col>
+          <v-row>
+            <v-col cols="12">
+              <v-row align="center" justify="center">
+                <v-text-field
+                  v-if="pageStatus=='edit'"
+                  v-model.trim="username"
+                  :label="you+name"
+                  :placeholder="namenotsent"
+                  class="shrink"
+                ></v-text-field>
+              </v-row>
+              <v-row align="center" justify="center">
+                <v-btn
+                  align="center"
+                  justify="center"
+                  v-if="pageStatus=='edit'"
+                  rounded
+                  color="primary"
+                  dark
+                  @click="changeParam"
+                >确定</v-btn>
+              </v-row>
+              <v-row align="center" justify="center">
+                <v-btn
+                  align="center"
+                  justify="center"
+                  v-if="pageStatus=='view'"
+                  rounded
+                  color="primary"
+                  dark
+                  @click="resetForm"
+                >制作我的</v-btn>
+              </v-row>
+              <v-row v-if="pageStatus=='ImgSharePageProcessing'" align="center" justify="center">
+                <v-img src="../public/QRcode.svg" max-width="100px"></v-img>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+      <v-main v-if="pageStatus=='ImgSharePage'">
+        <v-card>
+          <v-card-text>你可以直接分享本页面，也可以长按下图分享。</v-card-text>
+          <v-row>
+            <v-col cols="12">
+              <v-row align="center" justify="center">
+                <img :src="shareImgSrc" style="max-width:50%"/>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-main>
+    </v-flex>
   </v-app>
 </template>
 
@@ -58,7 +98,7 @@ export default {
 
   computed: {
     clickable: function () {
-      return this.viewonly ? "null" : "click";
+      return this.pageStatus == "view" ? "null" : "click";
     },
   },
   created: function () {
@@ -69,7 +109,7 @@ export default {
       window.location = window.location + "?weixin=crappy";
     } else if (window.location.href.indexOf("name=") != -1) {
       this.username = url.searchParams.get("name");
-      this.viewonly = true;
+      this.pageStatus = "view";
       this.achievements.forEach((element) => {
         let selectedArray = url.searchParams.get("selected");
         if (selectedArray.indexOf("" + element.id) != -1) {
@@ -89,7 +129,7 @@ export default {
       if (this.username == "") {
         this.username = this.me;
       }
-      this.viewonly = true;
+      this.pageStatus = "view";
       let newTitle = this.username + this.org;
       let newURL =
         window.location.href.split("?")[0] +
@@ -99,17 +139,27 @@ export default {
         selectedArr;
       history.pushState({}, newTitle, newURL);
       //window.location.hash = "#" + this.username + "," + selectedArr;
+      //console.log(this);
 
-      let node = document.getElementById("app");
-      toimg.toPng(node).then(function (dataUrl) {
-        var img = new Image();
-        img.src = dataUrl;
-        document.body.appendChild(img);
-      });
+      let thisapp = this;
+      let node = document.getElementById("forShare");
+      this.pageStatus = "ImgSharePageProcessing";
+
+      setTimeout(() => {
+        //load QR
+        toimg.toPng(node).then(function (dataUrl) {
+          //var img = document.getElementById("shareImg");
+          //console.log(this);
+          //console.log(thisapp);
+          thisapp.shareImgSrc = dataUrl;
+          thisapp.pageStatus = "ImgSharePage";
+          //document.body.appendChild(img);
+        });
+      }, 2000);
     },
     resetForm: function () {
       this.username = "";
-      this.viewonly = false;
+      this.pageStatus = "edit";
       this.achievements.forEach((element) => {
         element.selected = false;
       });
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import titleComponent from './title.vue';
 
 Vue.config.productionTip = false
-
+Vue.component('vue-title', titleComponent);
 new Vue({
   vuetify,
   render: h => h(App)
